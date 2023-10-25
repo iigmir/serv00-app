0f5102207391cdb6fa7027381b3ca5255aaedcc8
iigmir <roc120j@gmail.com>
2023-10-25 14:10:56 +0800

    Fetch API

--- public_html/api/blog-metadata.php
+++ public_html/api/blog-metadata.php
@@ -3,10 +3,33 @@
 class BlogMetadata
 {
     protected $id;
+    protected $data = null;
     private $apiurl = "https://api.github.com/repos/iigmir/blog-source/contents/info-files/articles.json";
+    /**
+     * @see <https://docs.github.com/en/rest/overview/resources-in-the-rest-api?apiVersion=2022-11-28#user-agent-required>
+     */
+    private $useragent = "Mozilla/5.0 iigmir-serv00-app/1.0.0";
     public function __construct($id = null)
     {
         $this->id = $id;
+    }
+    public function fetch_api()
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->apiurl);
+        curl_setopt($ch, CURLOPT_USERAGENT, $this->useragent);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $this->data = curl_exec($ch);
+        curl_close($ch);
+    }
+    public function api_data()
+    {
+        $response = json_decode($this->data);
+        if( isset($response) )
+        {
+            return $response->content;
+        }
+        return null;
     }
 }
 
@@ -15,8 +38,8 @@ class BlogData extends BlogMetadata
     public function __construct()
     {
         $id = isset($_GET["id"]) ? $_GET["id"] : null;
-        // $this->id = $id;
         $this->metadata = new BlogMetadata($id);
+        $this->metadata->fetch_api();
     }
     private function message(): string
     {
@@ -47,6 +70,7 @@ class BlogData extends BlogMetadata
         return array(
             "message" => $this->message(),
             "id" => $this->metadata->id,
+            "data" => $this->metadata->api_data(),
         );
     }
 }
